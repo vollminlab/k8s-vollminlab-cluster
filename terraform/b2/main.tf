bc10bed4f4f9bc0afa7df069bd33735f18e1a7c9
svollmi1 <scottvollmin@gmail.com>
2026-05-23 11:17:16 -0400

    feat(volsync): deploy operator and provision B2 bucket with scoped key (#728)

--- terraform/b2/main.tf
+++ terraform/b2/main.tf
@@ -3,3 +3,14 @@ resource "b2_bucket" "velero" {
   bucket_type = "allPrivate"
 }
 
+resource "b2_bucket" "volsync" {
+  bucket_name = "vollminlab-k8s-volsync"
+  bucket_type = "allPrivate"
+}
+
+resource "b2_application_key" "volsync" {
+  key_name     = "vollminlab-k8s-volsync"
+  capabilities = ["listBuckets", "listFiles", "readFiles", "writeFiles", "deleteFiles"]
+  bucket_id    = b2_bucket.volsync.id
+}
+
