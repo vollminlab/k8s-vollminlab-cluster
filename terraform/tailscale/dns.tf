d4af0e57ffbd37aa7f174dac172d854d0920f50f
svollmi1 <scottvollmin@gmail.com>
2026-05-24 12:03:25 -0400

    fix(tailscale): add vollm.in to split DNS so short links work on tailnet (#752)

--- terraform/tailscale/dns.tf
+++ terraform/tailscale/dns.tf
@@ -2,3 +2,8 @@ resource "tailscale_dns_split_nameservers" "vollminlab" {
   domain      = "vollminlab.com"
   nameservers = ["192.168.100.4"]
 }
+
+resource "tailscale_dns_split_nameservers" "vollm_in" {
+  domain      = "vollm.in"
+  nameservers = ["192.168.100.4"]
+}
